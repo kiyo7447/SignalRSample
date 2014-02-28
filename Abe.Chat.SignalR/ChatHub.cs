1dd5cd20a894d158e3ef7a0581496398216143ed
kiyo7447 <kiyogomi@gmail.com>
2014-02-28 10:48:30 +0900

    新規登録

---
+++ Abe.Chat.SignalR/ChatHub.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Abe.Chat
+{
+	[HubName("/system/chat")]
+	public class ChatHub : Hub
+	{
+		static int counter = 0;
+		public void SendAll(string text)
+		{
+			counter++;
+			Clients.All.Receive(counter + ":" + text);
+		}
+	}
+
+	[HubName("/system/chat2")]
+	public class ChatHub2 : Hub
+	{
+		static int counter = 0;
+		public void SendAll(string text, int num)
+		{
+			counter++;
+			Clients.All.Receive(counter + ":" + text + "*" + num, "hogehgoe");
+		}
+	}
+
+	[HubName("echo")]
+	public class EchoHub : Hub
+	{
+		public void Send(string text)
+		{
+			Clients.All.Receive(text);
+		}
+	}
+}
---
+++ Abe.Chat.SignalR/ReadMe.txt
@@ -0,0 +1,6 @@
+﻿http://www.atmarkit.co.jp/ait/articles/1303/19/news099.html
+
+
+1)
+
+Install-Package Microsoft.AspNet.SignalR
